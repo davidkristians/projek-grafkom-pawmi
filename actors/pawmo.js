2c8eb3c6ad3801bcc669baaf9af95a40dea2d967
aspiringbillionaire <ojodnah.newo@gmail.com>
2025-10-23 14:45:56 +0700

    animsaikontol

--- actors/pawmo.js
+++ actors/pawmo.js
@@ -1,5 +1,3 @@
-// actors/pawmo.js
-
 import { animatePawmo } from '../animations/pawmo.js';
 
 // ▼▼▼ DIUBAH: Import pawmoTorso, HAPUS yang curved ▼▼▼
@@ -14,92 +12,115 @@ import { pawmoFoot } from "../geometry2/pawmo-foot.js";
 import { pawmoTuft } from "../geometry2/pawmo-tuft.js";
 import { paraboloid } from "../geometry2/paraboloid.js"; // Butuh untuk pawmo-foot
 
-// Fungsi patchRenderPrototype (Tetap sama)
+// Fungsi patchRenderPrototype (Untuk Geometri yang digambar)
 function patchRenderPrototype(proto, normMatLoc) {
-    proto.render = function(PARENT_MATRIX) {
-        const M = LIBS.get_I4();
-        LIBS.mul(M, PARENT_MATRIX, this.POSITION_MATRIX);
-        LIBS.mul(M, M, this.MOVE_MATRIX);
-        this.GL.useProgram(this.SHADER_PROGRAM);
-        this.GL.uniformMatrix4fv(this._MMatrix, false, M);
-        this.GL.uniformMatrix4fv(normMatLoc, false, M);
-        this.GL.bindBuffer(this.GL.ARRAY_BUFFER, this.OBJECT_VERTEX);
-        this.GL.vertexAttribPointer(this._position, 3, this.GL.FLOAT, false, 36, 0);
-        this.GL.enableVertexAttribArray(this._position);
-        this.GL.vertexAttribPointer(this._color, 3, this.GL.FLOAT, false, 36, 12);
-        this.GL.enableVertexAttribArray(this._color);
-        this.GL.vertexAttribPointer(this._normal, 3, this.GL.FLOAT, false, 36, 24);
-        this.GL.enableVertexAttribArray(this._normal);
-        this.GL.bindBuffer(this.GL.ELEMENT_ARRAY_BUFFER, this.OBJECT_FACES);
-        this.GL.drawElements(this.GL.TRIANGLES, this.faces.length, this.GL.UNSIGNED_SHORT, 0);
-        if (this.childs && this.childs.length > 0) {
-            this.childs.forEach(c => c.render(M));
-        }
-    }
+    proto.render = function(PARENT_MATRIX) {
+        const M = LIBS.get_I4();
+        LIBS.mul(M, PARENT_MATRIX, this.POSITION_MATRIX);
+        LIBS.mul(M, M, this.MOVE_MATRIX);
+        this.GL.useProgram(this.SHADER_PROGRAM);
+        this.GL.uniformMatrix4fv(this._MMatrix, false, M);
+        this.GL.uniformMatrix4fv(normMatLoc, false, M);
+        this.GL.bindBuffer(this.GL.ARRAY_BUFFER, this.OBJECT_VERTEX);
+        this.GL.vertexAttribPointer(this._position, 3, this.GL.FLOAT, false, 36, 0);
+        this.GL.enableVertexAttribArray(this._position);
+        this.GL.vertexAttribPointer(this._color, 3, this.GL.FLOAT, false, 36, 12);
+        this.GL.enableVertexAttribArray(this._color);
+        this.GL.vertexAttribPointer(this._normal, 3, this.GL.FLOAT, false, 36, 24);
+        this.GL.enableVertexAttribArray(this._normal);
+        this.GL.bindBuffer(this.GL.ELEMENT_ARRAY_BUFFER, this.OBJECT_FACES);
+        this.GL.drawElements(this.GL.TRIANGLES, this.faces.length, this.GL.UNSIGNED_SHORT, 0);
+        if (this.childs && this.childs.length > 0) {
+            this.childs.forEach(c => c.render(M));
+        }
+    }
 }
+
+// ▼▼▼ PERBAIKAN: Fungsi patchRenderPrototype BARU khusus untuk GROUP ▼▼▼
+// Group tidak digambar, hanya mengalikan matriks dan me-render anaknya
+function patchGroupRenderPrototype(proto) {
+    proto.render = function(PARENT_MATRIX) {
+        const M = LIBS.get_I4();
+        // Gabungkan matriks induk dengan POSISI & GERAKAN group ini
+        LIBS.mul(M, PARENT_MATRIX, this.POSITION_MATRIX);
+        LIBS.mul(M, M, this.MOVE_MATRIX);
+        
+        // Panggil render untuk semua anak dengan matriks baru (M)
+        if (this.childs && this.childs.length > 0) {
+            this.childs.forEach(c => c.render(M));
+        }
+    }
+}
+// ▲▲▲ SELESAI FUNGSI BARU ▲▲▲
 
 // Fungsi utama pembuat Pawmo
 export function createPawmo(animationLoop) {
-    const app = window.myApp;
-    if (!app || !app.gl) { console.error("Aplikasi belum siap!"); return; }
-    const GL = app.gl;
-    const SHADER_PROGRAM = app.mainProgram;
-    const {posLoc: _pos, colLoc: _col, normLoc: _normal, mvLoc: _Mmatrix, normMatLoc: _NMatrixLoc} = app;
+    const app = window.myApp;
+    if (!app || !app.gl) { console.error("Aplikasi belum siap!"); return; }
+    const GL = app.gl;
+    const SHADER_PROGRAM = app.mainProgram;
+    const {posLoc: _pos, colLoc: _col, normLoc: _normal, mvLoc: _Mmatrix, normMatLoc: _NMatrixLoc} = app;
 
-    // ▼▼▼ DIUBAH: Patch pawmoTorso, HAPUS segment ▼▼▼
-    patchRenderPrototype(ellipsoid.prototype, _NMatrixLoc);
-    patchRenderPrototype(cone.prototype, _NMatrixLoc);
-    patchRenderPrototype(paraboloid.prototype, _NMatrixLoc); // Patch paraboloid untuk bantalan kaki
-    patchRenderPrototype(pawmoTuft.prototype, _NMatrixLoc);
-    patchRenderPrototype(pawmoTorso.prototype, _NMatrixLoc); // Patch torso lama
-    patchRenderPrototype(PawmoArm.prototype, _NMatrixLoc);
-    // pawmoFoot tidak perlu di-patch karena render-nya sudah di-override manual
+    // ▼▼▼ PERBAIKAN: Gunakan patchGroupRenderPrototype untuk group ▼▼▼
+    patchGroupRenderPrototype(group.prototype);
 
-    const PAWMO_ORANGE = [236/255, 187/255, 98/255];
-    const PAWMO_WHITE = [238/255, 238/255, 238/255];
+    // ▼▼▼ DIUBAH: Patch sisanya ▼▼▼
+    patchRenderPrototype(ellipsoid.prototype, _NMatrixLoc);
+    patchRenderPrototype(cone.prototype, _NMatrixLoc);
+    patchRenderPrototype(paraboloid.prototype, _NMatrixLoc); // Patch paraboloid untuk bantalan kaki
+    patchRenderPrototype(pawmoTuft.prototype, _NMatrixLoc);
+    patchRenderPrototype(pawmoTorso.prototype, _NMatrixLoc); // Patch torso lama
+    patchRenderPrototype(PawmoArm.prototype, _NMatrixLoc);
+    // pawmoFoot tidak perlu di-patch karena render-nya sudah di-override manual
 
-    const PawmoRig = new group(_Mmatrix, _normal);
+    const PAWMO_ORANGE = [236/255, 187/255, 98/255];
+    const PAWMO_WHITE = [238/255, 238/255, 238/255];
 
-    // ▼▼▼ DIUBAH: Gunakan pawmoTorso ▼▼▼
-    const Torso = new pawmoTorso(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, { color: PAWMO_ORANGE });
-    const Left_Hand = new pawmoHand(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, { orange: PAWMO_ORANGE, white: PAWMO_WHITE });
-    const Right_Hand = new pawmoHand(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, { orange: PAWMO_ORANGE, white: PAWMO_WHITE });
-    // ▼▼▼ DIUBAH: Teruskan NMatrixLoc ke pawmoFoot ▼▼▼
-    const Left_Foot = new pawmoFoot(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, _NMatrixLoc, { color: PAWMO_ORANGE });
-    const Right_Foot = new pawmoFoot(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, _NMatrixLoc, { color: PAWMO_ORANGE });
-    const Tail = new pawmoTail(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, {});
-    const Head = new pawmoHead(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, {});
+    const PawmoRig = new group(_Mmatrix, _normal);
 
-    // ▼▼▼ DIUBAH: Gunakan positioning dari main.js lama ▼▼▼
-    LIBS.set_I4(Left_Hand.POSITION_MATRIX); LIBS.translateX(Left_Hand.POSITION_MATRIX, -1.5); LIBS.translateY(Left_Hand.POSITION_MATRIX, 0.7); LIBS.rotateZ(Left_Hand.POSITION_MATRIX, LIBS.degToRad(120)); LIBS.rotateY(Left_Hand.POSITION_MATRIX, LIBS.degToRad(20));
-    LIBS.set_I4(Right_Hand.POSITION_MATRIX); LIBS.translateX(Right_Hand.POSITION_MATRIX, 1.5); LIBS.translateY(Right_Hand.POSITION_MATRIX, 0.7); LIBS.rotateZ(Right_Hand.POSITION_MATRIX, LIBS.degToRad(-120)); LIBS.rotateY(Right_Hand.POSITION_MATRIX, LIBS.degToRad(-20));
-    LIBS.set_I4(Left_Foot.POSITION_MATRIX); LIBS.translateX(Left_Foot.POSITION_MATRIX, -0.6); LIBS.translateY(Left_Foot.POSITION_MATRIX, -1.7); LIBS.translateZ(Left_Foot.POSITION_MATRIX, 0.4);
-    LIBS.set_I4(Right_Foot.POSITION_MATRIX); LIBS.translateX(Right_Foot.POSITION_MATRIX, 0.6); LIBS.translateY(Right_Foot.POSITION_MATRIX, -1.7); LIBS.translateZ(Right_Foot.POSITION_MATRIX, 0.4);
-    LIBS.set_I4(Tail.POSITION_MATRIX); LIBS.rotateY(Tail.POSITION_MATRIX, LIBS.degToRad(90)); LIBS.translateY(Tail.POSITION_MATRIX, -1.2); LIBS.translateZ(Tail.POSITION_MATRIX, -1.4);
-    LIBS.set_I4(Head.POSITION_MATRIX); LIBS.translateY(Head.POSITION_MATRIX, 2.0); // <-- Posisi kepala sederhana
+    // ▼▼▼ DIUBAH: Gunakan pawmoTorso ▼▼▼
+    const Torso = new pawmoTorso(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, { color: PAWMO_ORANGE });
+    const Left_Hand = new pawmoHand(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, { orange: PAWMO_ORANGE, white: PAWMO_WHITE });
+    const Right_Hand = new pawmoHand(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, { orange: PAWMO_ORANGE, white: PAWMO_WHITE });
+    // ▼▼▼ DIUBAH: Teruskan NMatrixLoc ke pawmoFoot ▼▼▼
+    const Left_Foot = new pawmoFoot(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, _NMatrixLoc, { color: PAWMO_ORANGE });
+    const Right_Foot = new pawmoFoot(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, _NMatrixLoc, { color: PAWMO_ORANGE });
+    const Tail = new pawmoTail(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, {});
+    const Head = new pawmoHead(GL, SHADER_PROGRAM, _pos, _col, _Mmatrix, _normal, {});
 
-    // ▼▼▼ DIUBAH: Penggabungan Rig sederhana ▼▼▼
-    PawmoRig.childs.push(Torso, Left_Hand, Right_Hand, Left_Foot, Right_Foot, Tail, Head);
+    // ▼▼▼ DIUBAH: Gunakan positioning dari main.js lama ▼▼▼
+    LIBS.set_I4(Left_Hand.POSITION_MATRIX); LIBS.translateX(Left_Hand.POSITION_MATRIX, -1.5); LIBS.translateY(Left_Hand.POSITION_MATRIX, 0.7); LIBS.rotateZ(Left_Hand.POSITION_MATRIX, LIBS.degToRad(120)); LIBS.rotateY(Left_Hand.POSITION_MATRIX, LIBS.degToRad(20));
+    LIBS.set_I4(Right_Hand.POSITION_MATRIX); LIBS.translateX(Right_Hand.POSITION_MATRIX, 1.5); LIBS.translateY(Right_Hand.POSITION_MATRIX, 0.7); LIBS.rotateZ(Right_Hand.POSITION_MATRIX, LIBS.degToRad(-120)); LIBS.rotateY(Right_Hand.POSITION_MATRIX, LIBS.degToRad(-20));
+    LIBS.set_I4(Left_Foot.POSITION_MATRIX); LIBS.translateX(Left_Foot.POSITION_MATRIX, -0.6); LIBS.translateY(Left_Foot.POSITION_MATRIX, -1.7); LIBS.translateZ(Left_Foot.POSITION_MATRIX, 0.4);
+    LIBS.set_I4(Right_Foot.POSITION_MATRIX); LIBS.translateX(Right_Foot.POSITION_MATRIX, 0.6); LIBS.translateY(Right_Foot.POSITION_MATRIX, -1.7); LIBS.translateZ(Right_Foot.POSITION_MATRIX, 0.4);
+    LIBS.set_I4(Tail.POSITION_MATRIX); LIBS.rotateY(Tail.POSITION_MATRIX, LIBS.degToRad(90)); LIBS.translateY(Tail.POSITION_MATRIX, -1.2); LIBS.translateZ(Tail.POSITION_MATRIX, -1.4);
+    LIBS.set_I4(Head.POSITION_MATRIX); LIBS.translateY(Head.POSITION_MATRIX, 2.0); // <-- Posisi kepala sederhana
 
-    // Simpan Referensi Animasi
-    PawmoRig.torsoRef = Torso;
-    PawmoRig.tailRef = Tail;
-    PawmoRig.mouthRef = Head.smileRef;
-    PawmoRig.leftHandMove = Left_Hand.MOVE_MATRIX;
-    PawmoRig.rightHandMove = Right_Hand.MOVE_MATRIX;
-    PawmoRig.headMove = Head.MOVE_MATRIX;
+    // ▼▼▼ DIUBAH: Penggabungan Rig sederhana ▼▼▼
+    PawmoRig.childs.push(Torso, Left_Hand, Right_Hand, Left_Foot, Right_Foot, Tail, Head);
 
-    PawmoRig.setup();
+    // Simpan Referensi Animasi
+    PawmoRig.torsoRef = Torso;
+    PawmoRig.tailRef = Tail;
+    PawmoRig.mouthRef = Head.smileRef;
+    PawmoRig.leftHandMove = Left_Hand.MOVE_MATRIX;
+    PawmoRig.rightHandMove = Right_Hand.MOVE_MATRIX;
+    PawmoRig.headMove = Head.MOVE_MATRIX;
+    PawmoRig.leftFootMove = Left_Foot.MOVE_MATRIX;
+    PawmoRig.rightFootMove = Right_Foot.MOVE_MATRIX;
 
-    // Posisikan Aktor LOKAL di atas pulau
-    LIBS.set_I4(PawmoRig.POSITION_MATRIX);
-    LIBS.translateY(PawmoRig.POSITION_MATRIX, 1.23);
-    LIBS.translateX(PawmoRig.POSITION_MATRIX, 0.5);
-    LIBS.translateZ(PawmoRig.POSITION_MATRIX, 0.5);
-    LIBS.scale(PawmoRig.POSITION_MATRIX, 0.2, 0.2, 0.2); 
+    PawmoRig.setup();
 
-    animationLoop.registerActorAnimation(PawmoRig, animatePawmo);
+    // Posisikan Aktor LOKAL di atas pulau
+    LIBS.set_I4(PawmoRig.POSITION_MATRIX);
+    LIBS.translateY(PawmoRig.POSITION_MATRIX, 2);
+    LIBS.translateX(PawmoRig.POSITION_MATRIX, 0.5);
+    LIBS.translateZ(PawmoRig.POSITION_MATRIX, 0.5);
+    LIBS.scale(PawmoRig.POSITION_MATRIX, 0.2, 0.2, 0.2); 
 
-    console.log("--- PAWMO SUKSES DIBUAT (Opsi A: Torso Sederhana) ---");
-    return PawmoRig;
+    animationLoop.registerActorAnimation(PawmoRig, animatePawmo);
+
+    console.log("--- PAWMO SUKSES DIBUAT (Opsi A: Torso Sederhana) ---");
+    return PawmoRig;
 }
+
--- animations/pawmo.js
+++ animations/pawmo.js
@@ -15,62 +15,112 @@ const PAWMO_TAIL_AMOUNT = 0.20;
 const PAWMO_MOUTH_SPEED = 3.5;
 const PAWMO_MOUTH_MAX_SCALE = 1.2;
 
+// Konstanta Kaki
+const PAWMO_WALK_SPEED = 2.0; // Samakan dengan kecepatan tangan agar sinkron
+const PAWMO_WALK_STEP_HEIGHT = 0.15; // Seberapa tinggi kaki diangkat
+const PAWMO_WALK_STEP_LENGTH = 0.2;  // Seberapa jauh kaki melangkah ke depan/belakang
+const PAWMO_WALK_STEP_ROTATION = LIBS.degToRad(-20); // Rotasi "ujung kaki" ke atas
+const PAWMO_BODY_MOVE_SPEED = 0.4;
+
+// ▼▼▼ BARU: Tambahkan durasi untuk berjalan ▼▼▼
+const PAWMO_WALK_DURATION = 3.0; // Akan berjalan selama 3 detik saja
+
 // Fungsi animasi Pawmo
 export function animatePawmo(actor, time, deltaTime) {
-    const sinHand = Math.sin(time * PAWMO_HAND_SPEED);
-    const sinHead = Math.sin(time * PAWMO_HEAD_SPEED);
+    const sinHand = Math.sin(time * PAWMO_HAND_SPEED);
+    const sinHead = Math.sin(time * PAWMO_HEAD_SPEED);
+    // sinWalk dipindah ke dalam blok 'if' di bawah
 
-    // Animasi Ekor (Sudah benar, kanan-kiri)
-    if (actor.tailRef) {
-        const tailRotation = Math.sin(time * PAWMO_TAIL_SPEED) * PAWMO_TAIL_AMOUNT;
-        LIBS.set_I4(actor.tailRef.MOVE_MATRIX);
-        LIBS.rotateY(actor.tailRef.MOVE_MATRIX, tailRotation);
-    }
+    // --- Animasi Idle (Selalu Berjalan) ---
 
-    // Animasi Mulut (Tidak berubah)
-    if (actor.mouthRef) {
-        const scaleT = (Math.sin(time * PAWMO_MOUTH_SPEED) + 1) / 2;
-        const currentScale = 1.0 + (PAWMO_MOUTH_MAX_SCALE - 1.0) * scaleT;
-        LIBS.set_I4(actor.mouthRef.MOVE_MATRIX);
-        LIBS.scale(actor.mouthRef.MOVE_MATRIX, currentScale, currentScale, currentScale);
-    }
+    // Animasi Ekor (Sudah benar, kanan-kiri)
+    if (actor.tailRef) {
+        const tailRotation = Math.sin(time * PAWMO_TAIL_SPEED) * PAWMO_TAIL_AMOUNT;
+        LIBS.set_I4(actor.tailRef.MOVE_MATRIX);
+        LIBS.rotateY(actor.tailRef.MOVE_MATRIX, tailRotation);
+    }
 
-    // Animasi Tangan Kiri (Tidak berubah)
-    if (actor.leftHandMove) {
-        const rotZ = sinHand * PAWMO_HAND_ROT_AMPLITUDE;
-        LIBS.set_I4(actor.leftHandMove);
-        LIBS.rotateZ(actor.leftHandMove, rotZ);
-        LIBS.translateX(actor.leftHandMove, -rotZ - 0.2);
-    }
+    // Animasi Mulut (Tidak berubah)
+    if (actor.mouthRef) {
+        const scaleT = (Math.sin(time * PAWMO_MOUTH_SPEED) + 1) / 2;
+        const currentScale = 1.0 + (PAWMO_MOUTH_MAX_SCALE - 1.0) * scaleT;
+        LIBS.set_I4(actor.mouthRef.MOVE_MATRIX);
+        LIBS.scale(actor.mouthRef.MOVE_MATRIX, currentScale, currentScale, currentScale);
+    }
 
-    // Animasi Tangan Kanan (Tidak berubah)
-    if (actor.rightHandMove) {
-        const rotZ = sinHand * PAWMO_HAND_ROT_AMPLITUDE;
-        LIBS.set_I4(actor.rightHandMove);
-        LIBS.rotateX(actor.rightHandMove, rotZ+0.2);
-        LIBS.rotateZ(actor.rightHandMove, +rotZ);
-        LIBS.rotateY(actor.rightHandMove, 0.1);
-        LIBS.translateY(actor.rightHandMove, -rotZ );
-        LIBS.translateX(actor.rightHandMove,-rotZ + 0.05);
-    }
+    // Animasi Tangan Kiri (Tidak berubah)
+    if (actor.leftHandMove) {
+        const rotZ = sinHand * PAWMO_HAND_ROT_AMPLITUDE;
+        LIBS.set_I4(actor.leftHandMove);
+        LIBS.rotateZ(actor.leftHandMove, rotZ);
+        LIBS.translateX(actor.leftHandMove, -rotZ - 0.2);
+    }
 
-    // ▼▼▼ ANIMASI KEPALA BARU ▼▼▼
-    if (actor.headMove) {
-        // Gerakan memantul naik-turun (tetap ada, tapi amplitudonya dikurangi)
-        const transY = Math.abs(sinHead) * PAWMO_HEAD_TRANS_AMPLITUDE;
-        
-        // Ganti nama variabel dari 'nodRotX' menjadi 'swayRotZ' agar jelas
-        const swayRotZ = sinHead * PAWMO_HEAD_SWAY_AMPLITUDE;
+    // Animasi Tangan Kanan (Tidak berubah)
+    if (actor.rightHandMove) {
+        const rotZ = sinHand * PAWMO_HAND_ROT_AMPLITUDE;
+        LIBS.set_I4(actor.rightHandMove);
+        LIBS.rotateX(actor.rightHandMove, rotZ+0.2);
+        LIBS.rotateZ(actor.rightHandMove, +rotZ);
+        LIBS.rotateY(actor.rightHandMove, 0.1);
+        LIBS.translateY(actor.rightHandMove, -rotZ );
+        LIBS.translateX(actor.rightHandMove,-rotZ + 0.05);
+    }
 
-        // Reset matriks gerakan
-        LIBS.set_I4(actor.headMove);
-        
-        // Terapkan gerakan memantul
-        LIBS.translateY(actor.headMove, transY);
-        
-        // Ganti rotateX menjadi rotateZ untuk gerakan mengayun kanan-kiri
-        LIBS.rotateZ(actor.headMove, swayRotZ);
-    }
-    // ▲▲▲ SELESAI ANIMASI KEPALA BARU ▲▲▲
+    // Animasi Kepala
+    if (actor.headMove) {
+        // Gerakan memantul naik-turun
+        const transY = Math.abs(sinHead) * PAWMO_HEAD_TRANS_AMPLITUDE;
+        // Gerakan mengayun kanan-kiri
+        const swayRotZ = sinHead * PAWMO_HEAD_SWAY_AMPLITUDE;
+        // Reset matriks gerakan
+        LIBS.set_I4(actor.headMove);
+        // Terapkan gerakan
+        LIBS.translateY(actor.headMove, transY);
+        LIBS.rotateZ(actor.headMove, swayRotZ);
+    }
+    // ▲▲▲ Selesai Animasi Idle ▲▲▲
+
+
+    // --- Animasi Berjalan (Hanya jika time < durasi) ---
+    if (time < PAWMO_WALK_DURATION) {
+        // Pindahkan kalkulasi sinWalk ke SINI
+        const sinWalk = Math.sin(time * PAWMO_WALK_SPEED);
+
+        // ▼▼▼ ANIMASI KAKI KANAN ▼▼▼
+        if (actor.rightFootMove) {
+            const transZ = sinWalk * PAWMO_WALK_STEP_LENGTH;
+            const transY = Math.max(0, sinWalk) * PAWMO_WALK_STEP_HEIGHT;
+            const rotX = Math.max(0, sinWalk) * PAWMO_WALK_STEP_ROTATION;
+            LIBS.set_I4(actor.rightFootMove);
+            LIBS.translateY(actor.rightFootMove, transY);
+            LIBS.translateZ(actor.rightFootMove, transZ);
+            LIBS.rotateX(actor.rightFootMove, rotX);
+        }
+
+        // ▼▼▼ ANIMASI KAKI KIRI ▼▼▼
+        if (actor.leftFootMove) {
+            const transZ = -sinWalk * PAWMO_WALK_STEP_LENGTH;
+            const transY = Math.max(0, -sinWalk) * PAWMO_WALK_STEP_HEIGHT;
+            const rotX = Math.max(0, -sinWalk) * PAWMO_WALK_STEP_ROTATION;
+            LIBS.set_I4(actor.leftFootMove);
+            LIBS.translateY(actor.leftFootMove, transY);
+            LIBS.translateZ(actor.leftFootMove, transZ);
+            LIBS.rotateX(actor.leftFootMove, rotX);
+        }
+
+        // ▼▼▼ ANIMASI GERAKAN SELURUH BADAN (LOKOMOSI) ▼▼▼
+        LIBS.translateZ(actor.POSITION_MATRIX, PAWMO_BODY_MOVE_SPEED * deltaTime);
+
+    } else {
+        // ▼▼▼ BARU: Reset posisi kaki jika sudah selesai berjalan ▼▼▼
+        // Ini menghentikan kaki di posisi netral (0,0,0)
+        if (actor.rightFootMove) {
+            LIBS.set_I4(actor.rightFootMove);
+        }
+        if (actor.leftFootMove) {
+            LIBS.set_I4(actor.leftFootMove);
+        }
+    }
 }
 
